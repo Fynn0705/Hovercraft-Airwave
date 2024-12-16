406d1a901cb5d906addf9228fd03ba467f792153
Fynn0705 <Fynn_Bremer@outlook.de>
2024-12-16 12:11:33 +0100

    Update Hovercraft Airwave Programm.py

---
+++ Hovercraft Airwave Programm.py
@@ -0,0 +1,22 @@
+# Programm Hovercraft Team Airwave
+#Teilnehmer Hannes, Fynn, Kai
+#Ersterstellung 25.11.2024
+#
+
+
+
+from machine import Pin
+import time
+
+# Taster und LED konfigurieren
+button = Pin(19, Pin.IN, Pin.PULL_UP)  # Tastereingang mit Pull-up-Widerstand
+led0 = Pin(6, Pin.OUT)
+led0 = Pin(6, Pin.OUT)
+led0 = Pin(6, Pin.OUT)
+led0 = Pin(6, Pin.O
+led0 = Pin(6, Pin.OUT)  UT)
+led0 = Pin(6, Pin.OUT)
+led0 = Pin(6, Pin.OUT)
+led0 = Pin(6, Pin.OUT)
+led0 = Pin(6, Pin.OUT)
+led0 = Pin(6, Pin.OUT)  # LED-Ausgang
